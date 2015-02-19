5828466c4dd776243ed64d16e23c2152b8fc84be
thothbot <thothbot@gmail.com>
2015-02-19 19:36:18 +0300

    Updated dev demo

--- parallax/demo/dev/demo/deferredjs/C29DA636983000C005D58E9CF79DEC3A/47.cache.js
+++
@@ -1,9 +0,0 @@
-function iqc(){}
-function mqc(){}
-function pqc(){}
-function aqc(){hac.call(this)}
-function eqc(){eqc=GKc;dqc=new iqc}
-function rqc(){rqc=GKc;gqc=new mqc}
-function sqc(){sqc=GKc;hqc=new pqc}
-function _pc(a){var b;b=tlb()+vPc+tlb()+vPc+tlb();WIb(a,eJb(a.e,Mu(GS,MKc,1,[b])))}
-JU(1205,1,rMc);_.Db=function Zpc(){J9b(this.c,new aqc(this.b))};JU(1206,973,{},aqc);_.ob=function bqc(){var a,b,c,d;this.b=new PQb(40,dHb(this.H.n),1,10000);z6b(this.b.ab,2000);a=new mXb(15,64,32);this.c=new brb;for(b=0;b<100;b++){c=new T2b((eqc(),dqc));_pc(MZb(c));d=new _7b(a,c);M5b(d.ab,(0.5-tlb())*1000);N5b(d.ab,(0.5-tlb())*1000);z6b(d.ab,(0.5-tlb())*1000);MPb(this.I,d);Tqb(this.c,d)}BHb(this.H.n,new I3b(15658734),1)};_.pb=function cqc(a){var b,c,d,e;for(b=0;b<100;b++){e=Wu(Wqb(this.c,b),304);JZb(e.v,this.H.n)}for(c=0;c<100;c++){e=Wu(Wqb(this.c,c),304);d=new T2b((eqc(),dqc));_pc(MZb(d));e.v=d}pHb(this.H.n,this.I,this.b,null,false)};var dqc;JU(1207,1,{},iqc);_.Vd=function jqc(){return rqc(),gqc};_.Wd=function kqc(){return sqc(),hqc};var gqc,hqc;JU(1208,1,{},mqc);_.uc=function nqc(){return 'void main() {\n\n\tif ( mod ( gl_FragCoord.x, 4.0001 ) < 1.0 || mod ( gl_FragCoord.y, 4.0001 ) < 1.0 )\n\n\t\tgl_FragColor = vec4( [*], 1.0 );\n\n\telse\n\n\t\tgl_FragColor = vec4( 1.0 );\n\n}'};JU(1209,1,{},pqc);_.uc=function qqc(){return 'void main() {\n\n\tvec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );\n\tgl_Position = projectionMatrix * mvPosition;\n\n}'};var nO=akb(xRc,'MiscMemoryTestShaders$DemoScene',1206),qO=akb(xRc,'MiscMemoryTestShaders_Resources_default_InlineClientBundleGenerator',1207),oO=akb(xRc,'MiscMemoryTestShaders_Resources_default_InlineClientBundleGenerator$1',1208),pO=akb(xRc,'MiscMemoryTestShaders_Resources_default_InlineClientBundleGenerator$2',1209);FMc(Ii)(47);
